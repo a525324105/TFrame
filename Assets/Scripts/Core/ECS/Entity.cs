faea10d9d4bda4c4a890cffabcc63984c5c12766
ALEXTANG <574809918@qq.com>
2022-03-03 11:39:54 +0800

    ECS架构核心
    ECS架构核心

---
+++ Assets/Scripts/Core/ECS/Entity.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ECS
+{
+    public class Entity : ECSObject, IIndex
+    {
+        [SerializeField]
+        internal List<ECSComponent> Components = new List<ECSComponent>();
+        internal List<IUpdate> Updates = new List<IUpdate>();
+        internal bool InActive;
+        internal bool CanUpdate;
+        public int Index { get; set; } = -1;
+
+        public Entity()
+        {
+            System = ECSSystem.Instance;
+        }
+
+        internal void Execute()
+        {
+            for (int i = 0; i < Updates.Count; i++)
+            {
+                Updates[i].Update();
+            }
+        }
+
+        public T AddComponent<T>() where T : ECSComponent, new()
+        {
+            T component = System.Get<T>();
+            component.Entity = this;
+            component.System = System;
+            Components.Add(component);
+            component.Awake();
+            if (component is IUpdate update)
+            {
+                Updates.Add(update);
+            }
+            CanUpdate = Updates.Count > 0;
+            return component;
+        }
+
+        public ECSComponent AddComponent(ECSComponent component)
+        {
+            component.Entity = this;
+            component.System = System;
+            Components.Add(component);
+            component.Awake();
+            if (component is IUpdate update)
+            {
+                Updates.Add(update);
+            }
+            CanUpdate = Updates.Count > 0;
+            return component;
+        }
+
+        public T GetComponent<T>() where T : ECSComponent
+        {
+            for (int i = 0; i < Components.Count; i++)
+            {
+                if (Components[i] is T type)
+                {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+
+        public ECSComponent GetComponent(Type componentType)
+        {
+            for (int i = 0; i < Components.Count; i++)
+            {
+                if (Components[i].GetType() == componentType)
+                {
+                    return Components[i];
+                }
+            }
+
+            return null;
+        }
+
+        public T[] GetComponents<T>() where T : ECSComponent
+        {
+            List<T> elements = new List<T>();
+            for (int i = 0; i < Components.Count; i++)
+            {
+                if (Components[i] is T type)
+                {
+                    elements.Add(type);
+                }
+            }
+            return elements.ToArray();
+        }
+
+        public List<T> GetComponentsList<T>() where T : ECSComponent
+        {
+            List<T> elements = new List<T>();
+            for (int i = 0; i < Components.Count; i++)
+            {
+                if (Components[i] is T type)
+                {
+                    elements.Add(type);
+                }
+            }
+            return elements;
+        }
+
+        public ECSComponent[] GetComponents(Type comType)
+        {
+            List<ECSComponent> elements = new List<ECSComponent>();
+            for (int i = 0; i < Components.Count; i++)
+            {
+                {
+                    if (Components[i].GetType() == comType)
+                    {
+                        elements.Add(Components[i]);
+                    }
+                }
+            }
+            return elements.ToArray();
+        }
+
+
+        public override string ToString()
+        {
+            string str = "[";
+            for (int i = 0; i < Components.Count; i++)
+            {
+                str += Components[i].GetType().Name + ",";
+            }
+            str = str.TrimEnd(',');
+            str += "]";
+            return $"{GetType().Name} Components: {str}";
+        }
+    }
+}
---
+++ Assets/Scripts/Core/ECS/IUpdate.cs
@@ -0,0 +1,10 @@
+﻿namespace ECS
+{
+    /// <summary>
+    /// ECS组件更新接口（减少组件for循环开销）
+    /// </summary>
+    public interface IUpdate
+    {
+        void Update();
+    }
+}
